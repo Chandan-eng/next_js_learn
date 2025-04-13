b9c1eeabd7823fcb9dfbca9123801c187b828a74
Chandan Mishra <chandan.mishra@nagarro.com>
2025-04-13 22:08:03 +0530

    use nested routing

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,8 +1,11 @@
+'use client';
+import { useRouter } from 'next/navigation';
 const UserDashboard = () => {
+    const router = useRouter();
     return (
         <div className="container-fluid mx-auto bg p-4">
             <div className="flex items-center gap-8 mb-4 bg-green-200 p-4 rounded text-green-900 cursor-pointer">
-                <div>Home</div>
+                <div onClick={()=>{router.push('/dashboard/home')}}>Home</div>
                 <div>About</div>
                 <div>Contact</div>
                 </div>
