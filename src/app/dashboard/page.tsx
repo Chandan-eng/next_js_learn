0fabdced92afa288842746433b3e884034972b7c
Chandan Mishra <chandan.mishra@nagarro.com>
2025-04-13 20:23:47 +0530

    app dashboard with nav bar and first route.

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,14 @@
+const UserDashboard = () => {
+    return (
+        <div className="container-fluid mx-auto bg p-4">
+            <div className="flex items-center gap-8 mb-4 bg-green-200 p-4 rounded text-green-900 cursor-pointer">
+                <div>Home</div>
+                <div>About</div>
+                <div>Contact</div>
+                </div>
+        <h1>User Dashboard</h1>
+        {/* Add your dashboard content here */}
+        </div>
+    );
+    }
+export default UserDashboard;
